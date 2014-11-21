913800e7fe3ccc2f1e1cbad1592768bd709c1df4
MrLostLink <LostLink29@gmail.com>
2014-11-21 15:37:32 -0500

    Update 4

--- src/Assignment4/Player.java
+++ src/Assignment4/Player.java
@@ -1,5 +1,13 @@
 package Assignment4;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Player {
 	
 
@@ -8,9 +16,31 @@ public class Player {
 	}
 
 	
+	private static void writeToSerializedFile(File file, Player player) {
+	    try {
+	        ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(file));
+	        output.writeObject(player);
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
 	
-	//Save board(Save game)[prints dat. file]
-	
-	//Load board(Load game)[reads dat. file]
+	private static Player readFromSerializedFile(File file) {
+	    Player player = null;
+	    try {
+	        ObjectInputStream input = new ObjectInputStream(new FileInputStream(file));
+	        player = (Player) input.readObject();
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } catch (ClassNotFoundException e) {
+	       e.printStackTrace();
+	    }
+	    return player;
+	}
 
+	
 }
